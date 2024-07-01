7125e4dfd2eec41b5cdafb05276a942aa16522e1
pcechetto <pedrocechetto@hotmail.com>
2024-07-01 15:11:14 -0300

    implementando sistema de reward

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -5,6 +5,7 @@ export default class Customer {
   private _name: string = "";
   private _address!: Address;
   private _active: boolean = false;
+  private _rewardsPoints: number = 0;
 
   constructor(id: string, name: string) {
     this._id = id;
@@ -12,8 +13,16 @@ export default class Customer {
     this.validate();
   }
 
-  get name() {
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
     return this._name;
+  }
+
+  get rewardPoints(): number {
+    return this._rewardsPoints;
   }
 
   validate() {
@@ -43,6 +52,10 @@ export default class Customer {
 
   deactivate() {
     this._active = false;
+  }
+
+  addRewardPoints(points: number) {
+    this._rewardsPoints += points;
   }
 
   set Address(address: Address) {
